107de5b52796c815dd71a4dca79e053eb5cbec3e
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-08-17 23:35:34 -0500

    chore: clean code

---
+++ src/components/project/NavProject.tsx
@@ -0,0 +1,30 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuNavLink,
+} from "../ui/navigation-menu";
+import { Separator } from "../ui/separator";
+
+const NavProject = () => {
+  return (
+    <nav className="mt-14 relative">
+      <NavigationMenu className="px-20">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuNavLink active>Overview</NavigationMenuNavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuNavLink>Updates</NavigationMenuNavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuNavLink>Discussion</NavigationMenuNavLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Separator className="absolute bottom-0" />
+    </nav>
+  );
+};
+
+export default NavProject;
