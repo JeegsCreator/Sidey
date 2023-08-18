3efaadf069c82d22b035fdb97d415f74fca3b03a
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-08-17 23:47:10 -0500

    chore: add create project route

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,41 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import {
+  Session,
+  createRouteHandlerClient,
+} from "@supabase/auth-helpers-nextjs";
+import { type ClassValue, clsx } from "clsx";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+interface GetUserAndProfileInput {
+  cookieStore: ReadonlyRequestCookies;
+}
+interface GetUserAndProfileOutput {
+  user:
+    | {
+        session: Session;
+      }
+    | {
+        session: null;
+      };
+  profile: any;
+  error: any | null;
+}
+
+export async function getUserAndProfile({
+  cookieStore,
+}: GetUserAndProfileInput): Promise<GetUserAndProfileOutput> {
+  const supabase = createRouteHandlerClient({
+    cookies: () => cookieStore,
+  });
+  const { data: user, error } = await supabase.auth.getSession();
+  const { data: profile } = await supabase
+    .from("Profile")
+    .select()
+    .eq("id", user.session?.user?.id)
+    .single();
+  return { user, profile, error };
 }
