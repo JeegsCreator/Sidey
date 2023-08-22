5560a4050045b46bc348df830107d218c8e3d752
Jhonn Gutierrez <jhonngutierrez.contact@gmail.com>
2023-08-22 13:50:21 -0500

    feat(Profile): :construction: Redesign profile page

--- src/app/(main)/user/[username]/page.tsx
+++ src/app/(main)/user/[username]/page.tsx
@@ -2,6 +2,16 @@ import { Suspense } from "react";
 import { getUser } from "@/app/api/user/getUser";
 import ProjectsCard from "@/components/profile/ProjectsCard";
 import ProfileHeader from "@/components/profile/ProfileHeader";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LatestUpdates from "@/components/project/LatestUpdates";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 export default async function Page({
   params,
@@ -13,11 +23,29 @@ export default async function Page({
   });
 
   return (
-    <main>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProfileHeader user={user} />
-        <ProjectsCard projects={user.profileToProject} />
-      </Suspense>
+    <main className="grid grid-cols-3 grid-rows-4 p-12 gap-4 h-full">
+      <Card className="h-full">
+        <CardHeader>
+          <CardTitle>{user.name}</CardTitle>
+          <CardDescription>@{user.username}</CardDescription>
+        </CardHeader>
+      </Card>
+      <div className="row-start-2 row-end-5">
+        <LatestUpdates list={[]} />
+      </div>
+      <Card className="h-full col-start-2 col-end-4 row-start-1 row-end-5">
+        <CardHeader className="flex flex-row justify-between items-start">
+          <CardTitle className="inline">Projects</CardTitle>
+          <div className="relative flex items-start h-full">
+            <Input placeholder="Search..." />
+            <Search className="absolute right-3" />
+          </div>
+        </CardHeader>
+      </Card>
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
+      {/* <ProfileHeader user={user} />
+        <ProjectsCard projects={user.profileToProject} /> */}
+      {/* </Suspense> */}
     </main>
   );
 }
